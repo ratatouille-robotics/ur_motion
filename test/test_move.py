9d166c3ac2afb1d4cc5a2c79e2f03694136bf3a2
Mohith Sakthivel <mohithksakthivel@gmail.com>
2022-03-22 21:10:08 -0400

    Add log msgs for tests

--- test/test_move.py
+++ test/test_move.py
@@ -13,7 +13,11 @@ def run():
     robot_mg = RobotMoveGroup()
     # check movements with joint targets
     target_joint = [0, -np.pi / 2, np.pi / 2, 0, np.pi / 2, 0]
-    robot_mg.go_to_joint_state(target_joint, cartesian_path=False)
+    rospy.loginfo("Testing motion with joint space interpolation...")
+    if robot_mg.go_to_joint_state(target_joint, cartesian_path=False):
+        rospy.loginfo("Run successful.")
+    else:
+        rospy.logerr("Test failed!")
     # check movements with pose targets
     pose = Pose()
     pose.position.x = -0.6
@@ -23,15 +27,21 @@ def run():
     pose.orientation.y = 0.5
     pose.orientation.z = 0.5
     pose.orientation.w = -0.5
-    robot_mg.go_to_pose_goal(pose, cartesian_path=True)
+    rospy.loginfo("Testing motion with cartesian interpolation...")
+    if robot_mg.go_to_pose_goal(pose, cartesian_path=True):
+        rospy.loginfo("Run successful.")
+    else:
+        rospy.logerr("Test failed!")
     # check pose trajectory control
     pose_list = []
     for i in range(4):
         pose_list.append(copy.deepcopy(pose))
         pose_list[-1].position.y += 0.05 * (i + 1)
     duration_list = [1.5, 2.5, 3, 4]
+    rospy.loginfo("Testing cartesian position trajectory controller...")
     robot_mg.send_cartesian_pos_trajectory(pose_list, duration_list, wait=True)
     # check velocity trajectory control
+    rospy.loginfo("Testing cartesian twist controller...")
     twist_time = 5
     rot_speed = -0.05
     t_step = 0.008
@@ -45,6 +55,7 @@ def run():
         rospy.sleep(t_step)
     twist.linear.y = 0
     robot_mg.send_cartesian_vel_trajectory(twist)
+    rospy.loginfo("All tests completed.")
 
 
 if __name__ == "__main__":
