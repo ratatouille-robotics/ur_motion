111b28c0bf90b8109fcbc8e50d509881224afc1c
Nevin Valsaraj <nevin.valsaraj32@gmail.com>
2022-04-10 23:29:24 -0400

    Add parameter to disable gripper functions
    Add parameter to disable gripper functions when running inside
    simulator.

--- test/dispense_test_autonomous.py
+++ test/dispense_test_autonomous.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+from faulthandler import disable
 from multiprocessing.connection import wait
 import sys
 import rospy
@@ -15,7 +17,10 @@ from ratatouille_pose_transforms.transforms import PoseTransforms
 
 # taped container position pre grasp/home
 HOME_POSITION_JOINT = [0.733216, -2.303996, 2.222002, -3.058677, -0.733774, 3.14139]
-INGREDIENT_POSITION = {2: [0.01, -0.2699, 0.66, 0.7071068, 0.0, 0.0, 0.7071068]}
+INGREDIENT_POSITION = {
+    2: [0.01, -0.2699, 0.66, 0.7071068, 0.0, 0.0, 0.7071068],
+    4: [0.01, -0.2699, 0.66, 0.7071068, 0.0, 0.0, 0.7071068],
+}
 DISPENSE_POSITION = [-0.4698, 0.0, 0.2950, 0.5, -0.5, -0.5, 0.5]
 PRE_DISPENSE_POSITION_CARTESIAN = [-0.2698, 0.0, 0.0, 0.6950, 0.5, -0.5, -0.5, 0.5]
 PRE_DISPENSE_POSITION_JOINT = [
@@ -28,27 +33,7 @@ PRE_DISPENSE_POSITION_JOINT = [
 ]
 
 
-def get_marker_pose(ingredient_id):
-    marker_origin = Pose()
-    marker_origin.position.x = 0.00
-    marker_origin.position.y = 0.00
-    marker_origin.position.z = 0.00
-    quat = quaternion_from_euler(0, 0, 0)
-    marker_origin.orientation.x = quat[0]
-    marker_origin.orientation.y = quat[1]
-    marker_origin.orientation.z = quat[2]
-    marker_origin.orientation.w = quat[3]
-
-    target_pose_base = pose_transformer.transform_pose_to_frame(
-        pose_source=marker_origin,
-        header_frame_id="pregrasp_" + str(ingredient_id),
-        base_frame_id="base_link",
-    )
-
-    return target_pose_base
-
-
-def run():
+def run(disable_gripper: bool = False):
     rospy.init_node("ur5e_move_test")
     rate = rospy.Rate(10.0)
 
@@ -58,7 +43,8 @@ def run():
     ############################################################################
     # open gripper and go to home position
 
-    robot_mg.open_gripper() or sys.exit(1)
+    if not disable_gripper:
+        robot_mg.open_gripper() or sys.exit(1)
     robot_mg.go_to_joint_state(HOME_POSITION_JOINT) or sys.exit(1)
 
     print("Reached home position. Enter ingredient number:", end="")
@@ -79,7 +65,21 @@ def run():
     ############################################################################
     # get pregrasp pose for ingredient
 
-    target_pose = get_marker_pose(ingredient_id=ingredient_id)
+    marker_origin = Pose()
+    marker_origin.position.x = 0.00
+    marker_origin.position.y = 0.00
+    marker_origin.position.z = 0.00
+    quat = quaternion_from_euler(0, 0, 0)
+    marker_origin.orientation.x = quat[0]
+    marker_origin.orientation.y = quat[1]
+    marker_origin.orientation.z = quat[2]
+    marker_origin.orientation.w = quat[3]
+
+    target_pose = pose_transformer.transform_pose_to_frame(
+        pose_source=marker_origin,
+        header_frame_id="pregrasp_" + str(ingredient_id),
+        base_frame_id="base_link",
+    )
 
     rospy.loginfo("Target pose:")
     rospy.loginfo(target_pose.pose)
@@ -123,8 +123,8 @@ def run():
 
     ############################################################################
     # grip container
-
-    robot_mg.close_gripper(wait=True) or sys.exit(1)
+    if not disable_gripper:
+        robot_mg.close_gripper(wait=True) or sys.exit(1)
 
     print("Press any key to continue:")
     input()
@@ -212,7 +212,8 @@ def run():
 
     ############################################################################
     # release gripper
-    robot_mg.open_gripper() or sys.exit(1)
+    if not disable_gripper:
+        robot_mg.open_gripper() or sys.exit(1)
 
     ############################################################################
     # go back from container
@@ -232,7 +233,13 @@ def run():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--disable-gripper", help="Disable gripper commands", action="store_true"
+    )
+    args = parser.parse_args()
+
     try:
-        run()
+        run(disable_gripper=args.disable_gripper)
     except rospy.ROSInterruptException:
         sys.exit(1)
